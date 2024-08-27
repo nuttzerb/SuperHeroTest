7dd5645769db62f7f9fb26b02ff5fd5e1d45ff39
Gia Bao <nuttzerzxc@gmail.com>
2024-08-27 21:48:51 +0700

    add basic navmesh movement

--- SuperHeroTest/Assets/Scripts/PlayerMovement.cs
+++ SuperHeroTest/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,20 +11,41 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     private float _movementSpeed = 5f;
 
+    private NavMeshAgent _agent;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _agent = GetComponent<NavMeshAgent>();
     }
-    void Start()
+    void Update()
     {
-
+        ProcessClickToMoveInput();
     }
+
+    private void ProcessClickToMoveInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _agent.enabled = true;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out var hitInfo))
+            {
+                Vector3 randomOffset = Random.insideUnitCircle * 0.5f;
+                randomOffset.y = 0;
+                _agent.destination = hitInfo.point + randomOffset;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         _rb.MovePosition(_rb.position + new Vector3(_moveInput.x, 0, _moveInput.y) * _movementSpeed * Time.fixedDeltaTime);
     }
     void OnMove(InputValue value)
     {
+        _agent.enabled = false;
         _moveInput = value.Get<Vector2>();
+        Debug.Log(_moveInput);
     }
 }
