393de361cde2776ca01c23a903ba193493fdefd8
Gia Bao <nuttzerzxc@gmail.com>
2024-08-27 22:46:46 +0700

    rotate player

--- SuperHeroTest/Assets/Scripts/PlayerMovement.cs
+++ SuperHeroTest/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 _moveInput;
+    private Vector3 _moveInput;
     private Rigidbody _rb;
     private float _movementSpeed = 5f;
 
     private NavMeshAgent _agent;
+
+    [SerializeField] private PlayerVisual _playerVisual;
+    private float rotationSpeed = 555f; // Rotation speed in degrees per second
 
     void Awake()
     {
@@ -21,8 +24,19 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         ProcessClickToMoveInput();
+        FaceTarget();
     }
 
+    void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveInput * _movementSpeed * Time.fixedDeltaTime);
+    }
+    void OnMove(InputValue value)
+    {
+        _agent.enabled = false;
+        _moveInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        Debug.Log(_moveInput);
+    }
     private void ProcessClickToMoveInput()
     {
         if (Input.GetMouseButtonDown(0))
@@ -38,14 +52,24 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void FaceTarget()
     {
-        _rb.MovePosition(_rb.position + new Vector3(_moveInput.x, 0, _moveInput.y) * _movementSpeed * Time.fixedDeltaTime);
-    }
-    void OnMove(InputValue value)
-    {
-        _agent.enabled = false;
-        _moveInput = value.Get<Vector2>();
-        Debug.Log(_moveInput);
+        if (_agent.enabled)
+        {
+            Vector3 direction = (_agent.destination - transform.position).normalized;
+            if(direction!= Vector3.zero)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+                transform.rotation = Quaternion.Slerp(_playerVisual.transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (_moveInput.magnitude >= 0.1f)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(_moveInput);
+                transform.rotation = Quaternion.Slerp(_playerVisual.gameObject.transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
     }
 }
